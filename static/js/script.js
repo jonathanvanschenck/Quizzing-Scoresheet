1b0a23bd733eb722737d716249639f81392b000e
jonathanvanschenck <44685047+jonathanvanschenck@users.noreply.github.com>
2020-02-08 17:23:44 -0800

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
 setup = function() {
+    var d = new Date();
+    var dlist = d.toLocaleDateString().match(/(\d*)[/](\d*)[/](\d*)/);
     var wind = d3.select(".window");
     var mtable = d3.select("#meta-table");
     var table = d3.select("#table-cont");
@@ -8,7 +10,7 @@ setup = function() {
     var hmenu = d3.select("#h-menu");
     
     
-    var data_meta_h = [{label:"",l1: "a"},{label:"",l1: "b"},
+    var data_meta_h = [{label:"Date:",l1: "a"},{label:"PlaceHolder",l1: "b"},
                         {label:"",l1: "g"},{label:"Name",l1: "h"},
                         {label:"",l1: "c"},
                         {label:"Team",l1: "d"},{label:"Score",l1: "e"},{label:"Points",l1: "f"}]
@@ -30,6 +32,8 @@ setup = function() {
                 .attr("id",d => "M"+la[i]+"-"+d.l1);
     }
     
+    d3.select("#MH-a").classed("meta-bar",true)
+        .classed("meta-header",false).classed("meta-data",true);
     d3.select("#MA-a").classed("meta-bar",true).text("Meet:");
     d3.select("#MB-a").classed("meta-bar",true).text("Room:");
     d3.select("#MC-a").classed("meta-bar",true).text("Quiz #:");   
@@ -51,6 +55,13 @@ setup = function() {
                 .classed("meta-input",true)
         })
     });
+    d3.select("#MH-b").classed("meta-header",false).classed("meta-data",true)
+        .text("")
+        .append("input").attr("type","date")
+        .attr("value",dlist[3]+"-"+padl(dlist[1],"0",2)+"-"+padl(dlist[2],"0",2))
+        .attr("id","input-MH-b")
+        .classed("meta-input",true);
+    
     
     mtable.selectAll(".meta-data-row").each(function (d) {
         d3.select(this).selectAll("td").filter(function (d) {
@@ -65,8 +76,9 @@ setup = function() {
 
     var data_startpoints = [{content:"__", value:""},
                            {content:"20", value:"20"}] 
-    var data_h = [{content:"NJ", value:"|"},{content:"TF", value:"TF"}] 
+    var data_h = [{content:"No Jump", value:"|"},{content:"Team Foul", value:"TF"}] 
     var data_pmenu = [{content:"__", value:""},
+                      {content:"No Jump", value:"|"},
                       {content:"20", value:"20"},
                       {content:"30", value:"30"},
                       {content:"E", value:"E"},
@@ -75,10 +87,9 @@ setup = function() {
                       {content:"B10", value:"B10"},
                       {content:"BE", value:"BE"}]
     var data_smenu = [{content:"__", value:""},
-                      {content:"NJ", value:"|"},
                       {content:"F", value:"F"},
                       {content:"F-10", value:"F-10"},
-                      {content:"TF", value:"TF"},
+                      {content:"Team Foul", value:"TF"},
                       {content:"CA", value:"CA"},
                       {content:"CO", value:"CO"},
                       {content:"CO-10", value:"CO-10"}]
@@ -402,10 +413,6 @@ setup = function() {
     });
     
     
-    update_footer = function(element) {
-        q = d3.select(element);
-    }
-    
     
     sort_teams = function (a , b) {
         if (a.score < b.score) {
@@ -433,46 +440,6 @@ setup = function() {
         }
     }
     
-    update_meta = function() {
-        var team_list = [];
-        d3.selectAll('.team-input').each(function (d,i) {
-            var tn = d3.select(this).attr("class").match(/Team([ABC])/)[1];
-            team_list.push({
-                score: +d3.select("#"+tn+"F-20").text(),
-                score2: +d3.select("#"+tn+"F-23").text(),
-                t: tn,
-                name: get_input_value(d3.select("#"+tn+"H-b").select("input")),
-                order: i+1
-            });
-        });
-        team_list.sort(sort_teams);
-        var i;
-        team_list[0].order = 1;
-        for (i = 0 ; i < 2 ; i++) {
-            if (team_list[i+1].score === team_list[i].score) {
-                team_list[i+1].order = team_list[i].order
-            } else {
-                team_list[i+1].order = i+1+1;
-            }
-        }
-        d3.selectAll('.team-input').each(function (d,i) {
-            var tn = d3.select(this).attr("class").match(/Team([ABC])/)[1];
-            d3.select('#M'+tn+'-e').text(team_list[i].score)
-                .classed("TeamA",team_list[i].t === "A")
-                .classed("TeamB",team_list[i].t === "B")
-                .classed("TeamC",team_list[i].t === "C");
-            d3.select('#M'+tn+'-d').text(team_list[i].name)
-                .classed("TeamA",team_list[i].t === "A")
-                .classed("TeamB",team_list[i].t === "B")
-                .classed("TeamC",team_list[i].t === "C");
-            d3.select('#M'+tn+'-f').text(parse_points(team_list[i].score,team_list[i].order))
-                .classed("TeamA",team_list[i].t === "A")
-                .classed("TeamB",team_list[i].t === "B")
-                .classed("TeamC",team_list[i].t === "C");
-        });
-
-    }
-    
     update_score("A");
     update_score("B");
     update_score("C");
@@ -486,6 +453,7 @@ get_input_value = function (d3_input_obj) {
 clear = function() {
     d3.selectAll("tr").remove();
     setup();
+    d3.select("#uploaded-file-label").text("");
 };
 
 parse_score = function (score) {
@@ -498,6 +466,7 @@ parse_score = function (score) {
 };
 
 update_score = function(tn) {
+    //console.log("updating score: ",tn);
     var q = d3.select("#"+tn+"F-c")
     var cumsum = +q.text();
     //var tn = q.attr("id").match(/([ABC])F[-]\d*/)[1];
@@ -524,6 +493,7 @@ update_score = function(tn) {
 }
 
 update_CI = function(tn,qn) {
+    //console.log("updating ci: ",tn,qn);
     var csum = 0, isum = 0;
     d3.select(d3.select("#"+tn+qn+"-c").node().parentNode).selectAll(".question").filter(function (d) {
         return +d3.select(this).attr("id").match(/[ABC]\d*[-](\d*)/)[1] < 21;
@@ -539,12 +509,144 @@ update_CI = function(tn,qn) {
     d3.select("#"+tn+qn+"-d").text(isum);
 }
 
-
-render = function() {
-    var d = new Date();
-    console.log(d.toLocaleDateString());
+update_meta = function() {
+    //console.log("updating meta");
+    var team_list = [];
+    d3.selectAll('.team-input').each(function (d,i) {
+        var tn = d3.select(this).attr("class").match(/Team([ABC])/)[1];
+        team_list.push({
+            score: +d3.select("#"+tn+"F-20").text(),
+            score2: +d3.select("#"+tn+"F-23").text(),
+            t: tn,
+            name: get_input_value(d3.select("#"+tn+"H-b").select("input")),
+            order: i+1
+        });
+    });
+    team_list.sort(sort_teams);
+    var i;
+    team_list[0].order = 1;
+    for (i = 0 ; i < 2 ; i++) {
+        if (team_list[i+1].score === team_list[i].score) {
+            team_list[i+1].order = team_list[i].order
+        } else {
+            team_list[i+1].order = i+1+1;
+        }
+    }
+    d3.selectAll('.team-input').each(function (d,i) {
+        var tn = d3.select(this).attr("class").match(/Team([ABC])/)[1];
+        d3.select('#M'+tn+'-e').text(team_list[i].score)
+            .classed("TeamA",team_list[i].t === "A")
+            .classed("TeamB",team_list[i].t === "B")
+            .classed("TeamC",team_list[i].t === "C");
+        d3.select('#M'+tn+'-d').text(team_list[i].name)
+            .classed("TeamA",team_list[i].t === "A")
+            .classed("TeamB",team_list[i].t === "B")
+            .classed("TeamC",team_list[i].t === "C");
+        d3.select('#M'+tn+'-f').text(parse_points(team_list[i].score,team_list[i].order))
+            .classed("TeamA",team_list[i].t === "A")
+            .classed("TeamB",team_list[i].t === "B")
+            .classed("TeamC",team_list[i].t === "C");
+    });
 }
 
-d3.select("#clear-button").on("click", d => clear());
-d3.select("#render-button").on("click", d => render());
+update_all = function() {
+    let tlist = ["","A","B","C"];
+    for (let t = tlist.pop(); tlist.length > 0 ; t = tlist.pop()) {
+        update_score(t);
+        let qlist = ["","1","2","3","4","5"];
+        for (let q = qlist.pop(); qlist.length > 0; q = qlist.pop()) {
+            update_CI(t,q);
+            //console.log(t,q);
+        }
+    }
+    update_meta();
+}
+
+padl = function (str,pval,num) {
+    let str2 = str + "";
+    while (str2.length < num)
+        str2 = pval + str2;
+    return str2;
+}
+padr = function (str,pval,num) {
+    let str2 = str + "";
+    while (str2.length < num)
+        str2 = str2 + pval;
+    return str2;
+}
+
+render = function(ext) {
+    let res = [];
+    d3.selectAll("tr").filter(function (d) {
+        return !d3.select(this).classed("menu-row");
+    }).each(function (d) {
+        let row = []//"";
+        d3.select(this).selectAll('td').each(function (d,i) {
+            let inp = d3.select(this).selectAll("input");
+            if (inp.size() === 0) {
+                row.push(d3.select(this).text());// = row + d3.select(this).text() + ",";
+            } else {
+                row.push(get_input_value(inp));// = row + get_input_value(inp) + ",";
+            }
+        });
+        res.push(row.join(",") + "\n");//row.slice(0,-1)+"\n");
+    });
+    let blob = new Blob(res, {type: "text/plaintext;charset=utf-8"});
+    saveAs(blob, get_input_value(d3.select("#input-MH-b"))+"-"+padl(get_input_value(d3.select("#input-MC-b")),"0",3)+"."+ext);
+}
+
+load_file = function(input) {
+    let file = input.files[0];
+    //console.log(file.name.split('').reverse().join(''));
+    let ext = file.name.split('').reverse().join('')
+    if (ext.indexOf(".") === -1) {
+        alert("Invalid File Type");
+        return null;
+    }
+    ext = ext.slice(0,ext.indexOf('.')).split('').reverse().join('');
+    //console.log(ext);
+    if (!(ext === 'dat')) {
+        alert("Invalid File Type");
+        return null;
+    }
+    
+    let reader = new FileReader();
+    
+    reader.onload = function () {
+        let rows = reader.result.split("\n");
+        d3.selectAll("tr").filter(function (d) {
+            return !d3.select(this).classed("menu-row");
+        }).each(function (d,i) {
+            //console.log(i);
+            //console.log(rows[i]);
+            let row = rows[i].split(',');
+            d3.select(this).selectAll('td').each(function (d,j) {
+                let inp = d3.select(this).selectAll("input");
+                if (inp.size() === 0) {
+                    d3.select(this).text(row[j]);
+                } else {
+                    document.getElementById(inp.attr("id")).value = row[j];
+                }
+            });
+        });
+    };
+
+    clear();
+    reader.readAsText(file);
+    d3.select("#uploaded-file-label").text("From File: "+file.name);
+    document.getElementById("upload-input").value = '';
+    setTimeout(function(){ 
+        update_all();
+    }, 1000); // ugly, but functional
+}
+
+d3.select("#clear-button").on("click", function (d) {
+    if (confirm("Are you sure you want to clear this form?")) {
+        clear();
+    } else {}
+});
+d3.select("#update-button").on("click", d => update_all());
+d3.select("#render1-button").on("click", d => render('dat'));
+d3.select("#upload-button").on("click", d => document.getElementById("upload-input").click());
+d3.select("#upload-input").on("change", function (d) { load_file(this); });
 setup();
